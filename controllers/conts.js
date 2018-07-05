5eea64c2b8097f37f3c997396df28cb6e398806f
Giovanni <33281942+GdJdMT@users.noreply.github.com>
2018-07-05 17:14:10 +0200

    Add Helmet, session, Flash

--- controllers/conts.js
+++ controllers/conts.js
@@ -3,10 +3,11 @@ const Gym = require('../models/gym');
 
 exports.findLast = (city, limit, done) => {
   Trainer.find({city:city},'displayName code lastModified', {sort: {lastModified: -1}, limit:limit}, done);
-  }
+};
+
 exports.findList = (city, done) => {
-  Trainer.find({city:city},'displayName team code', {sort: {displayName: 1}}, done);
-}
+  Trainer.find({city:city},'displayName team code', {sort: {name: 1}}, done);
+};
 exports.findGyms = (city, done) => {
     let cities;
     if (city === 'test') {
@@ -17,8 +18,88 @@ exports.findGyms = (city, done) => {
     Gym.find({city:cities},'name city coords',{sort : {city : 1}}, done);
 }
 
+exports.getCity = (req, res) => {
+  const d = new Date();
+  const city = req.params.city || process.env.DEFCITY;
+  let last;
+  let list;
+  let gyms;
+  let blue = [];
+  let red = [];
+  let yellow = [];
+  this.findLast(city, 20, (err,data) => {if (err) {throw err;} else {last = data; complete()}});
+  this.findList(city, (err,data) => {if (err) {throw err;} else {
+    for (let t of data) {
+      if (t.team === 'blue') {
+        blue.push(t);
+      } else if (t.team === 'yellow') {
+        yellow.push(t);
+      } else {
+        red.push(t);
+      };
+    }
+    list=data;
+    complete();
+  }});
+  this.findGyms(city,(err,data) => {if (err) {throw err;} else {gyms = data; complete()}});
+  const complete = () => {
+    if (last && list && gyms) {
+      res.render('index', {
+        title:`${city.charAt(0).toUpperCase() + city.substr(1)} local group`,
+        moment: require('moment'),
+        last: last,
+        blue: blue,
+        yellow: yellow,
+        red: red,
+        gyms: gyms
+      });
+    }
+  }
+};
+
+exports.saveUser = (req,res) => {
+  const cities = process.env.CITIES; // add approved cities here
+  const [nick, team, code, path] = [...Object.values(req.body)];
+  const city = path ? path : process.env.DEFCITY //set a default city
+  if (cities.includes(city)) {
+    Trainer.findOne({name:nick.trim().toLowerCase()},(err,t) => {
+      if (err){throw err;} else {
+        if (!t) { t = new Trainer({}); }
+        t.name = nick ? nick.trim().toLowerCase() : t.name;
+        t.displayName = nick ? nick.trim() : t.displayName;
+        t.$addToSet = {city:city},
+        t.team = team ? team : t.team;
+        t.code = code ? code.trim() : t.code ? t.code : '';
+        t.createdOn = t.createdOn ? t.createdOn : new Date();
+        t.lastModified = new Date();
+        t.save((err,data) => {
+          if (err) {
+            if (err.name === 'MongoError' && err.code === 11000) {
+              req.flash('danger','Oh snap! It seems a player with this nickname is already signed up!');
+              return res.redirect('/'+city);
+            } else {
+              throw err;
+            }
+          } else {
+            if (!data) {
+              req.flash('danger','Uhm... something went fishy...');
+              return res.redirect('/'+city);
+            } else {
+              req.flash('success',`You're all set, ${nick}!`);
+              return res.redirect('/'+city);
+            }
+          }
+        });
+      }
+    });
+  } else {
+    req.flash('danger',`Sorry, ${city} may not be a registered city.`);
+    return res.redirect('/'+city);
+  }
+}
+
 /*
-USER THIS CODE TO ADD GYMS 
+USER THIS CODE TO ADD GYMS
 NOTE: all fields are case sensitive.
 const gyms = [
   {name: 'test1', city: 'test', coords:["34.01466338397385","-118.49057972431184"]},
--- server.js
+++ server.js
@@ -1,117 +1,41 @@
 const express = require('express');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('cookie-session');
 const dotenv = require('dotenv').config();
+
 const app = express();
+app.use(helmet())
 
 mongoose.connect(process.env.DB);
 const Trainer = require('./models/trainer');
-const Gym = require('./models/gym');
 const conts = require('./controllers/conts');
 const apiRoutes = require('./routes/api.js');
 
 app.use(express.static('public'));
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
+
+app.use(session({
+  name:'pogosession',
+  secret: process.env.SESSION_SECRET,
+  maxAge: 24 * 60 * 60 * 1000
+}));
+
+app.use(require('flash')());
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get("/readme", (req,res) => {
-  res.sendFile(__dirname + '/views/readme.html');
-})
-app.get("/test/:city?", (req, res) => {
-  res.sendFile(__dirname + '/views/test.html');
-});
-
-app.get("/:city?", (req, res) => {
-  const d = new Date();
-  const city = req.params.city || process.env.DEFCITY;
-  let last;
-  let list;
-  let gyms;
-  let blue = [];
-  let red = [];
-  let yellow = [];
-  conts.findLast(city, 20, (err,data) => {if (err) {throw err;} else {last = data; complete()}});
-  conts.findList(city, (err,data) => {if (err) {throw err;} else {
-    for (let t of data) {
-      if (t.team === 'blue') {
-        blue.push(t);
-      } else if (t.team === 'yellow') {
-        yellow.push(t);
-      } else {
-        red.push(t);
-      };
-    }
-    list=data;
-    complete();
-  }});
-  conts.findGyms(city,(err,data) => {if (err) {throw err;} else {gyms = data; complete()}});
-  const complete = () => {
-    if (last && list && gyms) {
-      res.render('index', { 
-        title: `${city.charAt(0).toUpperCase() + city.substr(1)} local group`, 
-        moment: require('moment'),
-        last: last,
-        blue: blue,
-        yellow: yellow,
-        red: red,
-        gyms: gyms
-      });
-    }
-  }
+app.get('/*', (req,res,next) => {
+  req.session.flash = [];
+  next();
 });
 
 apiRoutes(app);
 
-app.post('/new', (req,res) => {
-  const cities = process.env.CITIES; // add approved cities here
-  const [nick, team, code, path] = [...Object.values(req.body)];
-  const city = path ? path : process.env.DEFCITY //set a default city
-  if (cities.includes(city)) {
-    const update = {};
-    update.$addToSet = {city:city};
-    update.lastModified = new Date();
-    if (code) {
-      update.code = code.trim();
-    }
-    Trainer.findOneAndUpdate({name:nick.trim().toLowerCase()},update,(err,data) => {
-      if (err){throw err;} else {
-        if (!data) {
-          const t = new Trainer({
-            name: nick.trim().toLowerCase(),
-            displayName: nick.trim(),
-            city: city,
-            team: team,
-            code: code.trim(),
-            createdOn: new Date(),
-            lastModified: new Date()
-          });
-          t.save((err,data) => {
-            if (err) {
-              if (err.name === 'MongoError' && err.code === 11000) {
-                return res.status(500).send({success: false, message: 'Oh snap! It seems a player with this nickname is already signed up!' });
-              } else {
-                throw err;
-              }
-            } else {
-              if (!data) {
-                res.json({success: false, message:'Uhm... something went fishy...'});
-              } else {
-                res.json({success: true, message:`You're all set, ${nick}!`});
-              }
-            }
-          });
-        } else {
-          res.json({success: true, message:`Profile updated, ${nick}!`});
-        }
-      }
-    });
-  } else {
-    res.json({success: false, message:`Sorry, ${city} may not be a registered city.`});
-  }
-});
-
+app.get("/:city?", conts.getCity);
+app.post('/new', conts.saveUser);
 // listen for requests :)
 const listener = app.listen(3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
